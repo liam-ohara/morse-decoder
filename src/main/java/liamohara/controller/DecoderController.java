ae68861fb2645129a6630923377cad4da9cae1ee
Liam O'Hara <liam.ohara@gmail.com>
2024-10-26 20:53:36 +0100

    Feat: add DecoderController.

---
+++ src/main/java/liamohara/controller/DecoderController.java
@@ -0,0 +1,12 @@
+package liamohara.controller;
+
+import liamohara.service.DecoderService;
+
+public class DecoderController {
+
+    DecoderService decoderService;
+
+    public String decode(String morseCode) {
+        return "";
+    }
+}
