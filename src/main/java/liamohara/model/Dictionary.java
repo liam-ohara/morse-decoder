09ec38af078d0f9bfbf9d737f1489c65b61d21f3
Liam O'Hara <liam.ohara@gmail.com>
2024-10-26 18:06:35 +0100

    Feat: implement singleton constructor for Dictionary.

--- src/main/java/liamohara/model/Dictionary.java
+++ src/main/java/liamohara/model/Dictionary.java
@@ -1,4 +1,16 @@
 package liamohara.model;
 
 public class Dictionary {
+
+    private static Dictionary instance;
+
+    private Dictionary() {}
+
+    public static Dictionary getInstance() {
+        if (instance == null) {
+            instance = new Dictionary();
+        }
+        return instance;
+    }
+
 }
